f153b619f1b0b21c94a1386bcddbd1d9a26f00a6
Fernangel7 <cholofernandez990@gmail.com>
2026-02-06 07:44:42 -0700

    Remove unused files and implement linear regression calculations in new scripts

---
+++ asd2.py
@@ -0,0 +1,45 @@
+# Leer datos desde un archivo CSV
+with open("./data.csv", "r") as file:
+    # Leer y organizar en listas los datos
+    data = [line.split(",") for line in file.read().splitlines()]
+
+# quitar headers
+data = data[1:]
+
+# Filtar datos, limpieza, parseo de tipos y redondeo
+# columna altura = entradas; columna peso = weights
+entradas = [float(e[0]) / 100 for e in data]
+weights = [float(e[1]) for e in data]
+
+# Numero de entradas
+n = len(entradas)
+
+# Calcular sumatorias
+sx = sum(entradas)
+sy = sum(weights)
+sxy = sum(xi * yi for xi, yi in zip(entradas, weights))
+sx2 = sum(xi**2 for xi in entradas)
+
+# Calcular pendiente (m)
+m = (n * sxy - sx * sy) / (n * sx2 - sx**2)
+
+# Calcular intercepto (b)
+b = (sy - m * sx) / n
+
+# Imprimir resultados
+print("Pendiente m =", m)
+print("Intercepto b =", b)
+print(format(f"Ecuacion de la recta: y = {m} * x {'+' if b >=0 else '-'} {abs(b)}"))
+
+# nuevas predicciones
+pred = [
+    1.66,
+    1.84,
+    1.89
+]
+
+y_pred = [round(m * xi + b, 2) for xi in pred]
+
+# Imprimir nueva predicción
+for i in range(len(pred)):
+    print(format(f"Peso estimado para altura {pred[i]}m = {y_pred[i]} kg"))
