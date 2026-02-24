59f089cd51f472d04883d46278f1178cf00d6d0a
Fernangel7 <cholofernandez990@gmail.com>
2026-02-23 21:00:26 -0700

    Add data processing and linear regression implementation with visualization

---
+++ news/data_generator.py
@@ -0,0 +1,11 @@
+# generar datos aleatorios y contruir un csv en base a ellos
+
+def generate_data(count, type):
+    if type == "altura&pesos":
+        return
+    else: return
+        
+    return
+
+def save_data_to_file(filename, directory, data):
+    return
